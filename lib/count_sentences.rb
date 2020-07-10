ab50ce672d75b75baf9f209345e0e6a13711aa47
bbtrombone <bbtrombone@sbcglobal.net>
2020-07-10 15:56:02 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+     new_string = self.split(/[.!?]/)
+     final_string = new_string.reject { |string| string.empty?}
+     final_string.count
   end
 end
